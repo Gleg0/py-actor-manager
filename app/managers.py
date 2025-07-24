504508e0b361338d3d8a42ff8a682a8536579c57
Glego <glego200323@gmail.com>
2025-07-25 00:09:56 +0300

    Solution fix flake8

--- app/managers.py
+++ app/managers.py
@@ -11,15 +11,15 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            f'INSERT INTO {self.table_name} '
-            f'(first_name, last_name) VALUES (?, ?)',
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self.connection.commit()
 
     def all(self) -> list[Actor]:
         actor_format_cursor = self.connection.execute(
-            f'SELECT * FROM {self.table_name}'
+            f"SELECT * FROM {self.table_name}"
         )
         return [
             Actor(*row)
@@ -28,16 +28,16 @@ class ActorManager:
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.connection.execute(
-            f'UPDATE {self.table_name} '
-            f'SET first_name = ?, last_name = ? '
-            f'WHERE id = ?',
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?",
             (new_first_name, new_last_name, pk)
         )
         self.connection.commit()
 
     def delete(self, pk: int) -> None:
         self.connection.execute(
-            f'DELETE FROM {self.table_name} WHERE id = ?',
+            f"DELETE FROM {self.table_name} WHERE id = ?",
             (pk,)
         )
         self.connection.commit()
